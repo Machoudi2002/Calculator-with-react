16b65ed0809af7939404be1ce0e2c1b3ef1c1122
Machoudi <78318497+Machoudi2002@users.noreply.github.com>
2023-08-04 14:59:31 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Footer() {
+
+    return (
+        <footer>
+            by <a target="_blank" href="https://github.com/Machoudi2002">Machoudi</a>
+        </footer>
+    )
+}
